6124043aa9066811f1cc8d3f7aaaf772bc4fc421
Kaushiki-KS <kaushikik048@gmail.com>
2025-09-04 12:36:18 +0530

    change to p

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div id='main'>
-      <h1>Now I can render any React component on any DOM node I want using ReactDOM.render</h1>
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     </div>
   )
 }
